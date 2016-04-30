d5870598f0c5121fbb9db3fe53ea617c958ddd4a
gskerry <gregory.skerry@gmail.com>
2016-04-30 14:49:04 -0400

    add constructor, set owner

--- contracts/Showme.sol
+++ contracts/Showme.sol
@@ -4,6 +4,10 @@ contract Showme {
 	address public owner;
 	mapping (address => uint) balances;
 
+	function Showme(){
+		owner = msg.sender;
+	}
+
 	function getOwner() returns(address) {
 		return owner;
 	}
--- environments/development/contracts/ConvertLib.sol.js
+++ environments/development/contracts/ConvertLib.sol.js
@@ -8,7 +8,7 @@
     abi: [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"conversionRate","type":"uint256"}],"name":"convert","outputs":[{"name":"convertedAmount","type":"uint256"}],"type":"function"}],
     binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
     unlinked_binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
-    address: "0x9653fc454161bfba8216d95830a782684e04caa6",
+    address: "0xd9f6f7fef5799b5b8d1dc19a0ff5b65171865ad9",
     generated_with: "2.0.6",
     contract_name: "ConvertLib"
   };
