8c054427b513dbe5db7aa9374f621511631d26f3
gskerry <gregory.skerry@gmail.com>
2016-04-30 23:16:29 -0400

    UI tweaking

--- environments/development/build/app.js
+++ environments/development/build/app.js
@@ -5738,7 +5738,7 @@ var Pudding =
     abi: [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"conversionRate","type":"uint256"}],"name":"convert","outputs":[{"name":"convertedAmount","type":"uint256"}],"type":"function"}],
     binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
     unlinked_binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
-    address: "0xaa0207807f457876b8d596c699dbea1aab9ff137",
+    address: "0x60ac611e474c1345c75d465914d517d74f9a8120",
     generated_with: "2.0.6",
     contract_name: "ConvertLib"
   };
@@ -5802,77 +5802,10 @@ var Pudding =
 (function() {
 
   var contract_data = {
-    abi: [{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalanceInEth","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[],"type":"constructor"}],
-    binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101408061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b1600435600073aa0207807f457876b8d596c699dbea1aab9ff1376396e4ee3d6100fb84610092565b6100b160043560243533600160a060020a0316600090815260208190526040812054829010156100c3576100f5565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b92915050565b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac905056",
-    unlinked_binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101408061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b1600435600073__ConvertLib____________________________6396e4ee3d6100fb84610092565b6100b160043560243533600160a060020a0316600090815260208190526040812054829010156100c3576100f5565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b92915050565b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac905056",
-    address: "0xd205bc754d1da3f6182dc7c57da020fd0be10542",
-    generated_with: "2.0.6",
-    contract_name: "MetaCoin"
-  };
-
-  function Contract() {
-    if (Contract.Pudding == null) {
-      throw new Error("MetaCoin error: Please call load() first before creating new instance of this contract.");
-    }
-
-    Contract.Pudding.apply(this, arguments);
-  };
-
-  Contract.load = function(Pudding) {
-    Contract.Pudding = Pudding;
-
-    Pudding.whisk(contract_data, Contract);
-
-    // Return itself for backwards compatibility.
-    return Contract;
-  }
-
-  Contract.new = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("MetaCoin error: Please call load() first before calling new().");
-    }
-
-    return Contract.Pudding.new.apply(Contract, arguments);
-  };
-
-  Contract.at = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("MetaCoin error: lease call load() first before calling at().");
-    }
-
-    return Contract.Pudding.at.apply(Contract, arguments);
-  };
-
-  Contract.deployed = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("MetaCoin error: Please call load() first before calling deployed().");
-    }
-
-    return Contract.Pudding.deployed.apply(Contract, arguments);
-  };
-
-  if (typeof module != "undefined" && typeof module.exports != "undefined") {
-    module.exports = Contract;
-  } else {
-    // There will only be one version of Pudding in the browser,
-    // and we can use that.
-    window.MetaCoin = Contract;
-  }
-
-})();
-;
-
-// Factory "morphs" into a Pudding class.
-// The reasoning is that calling load in each context
-// is cumbersome.
-
-(function() {
-
-  var contract_data = {
     abi: [{"constant":false,"inputs":[],"name":"getSender","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"getOwner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"inputs":[],"type":"constructor"}],
     binary: "606060405260008054600160a060020a03191633179055606b8060226000396000f3606060405260e060020a60003504635e01eb5a8114602e578063893d20e81460355780638da5cb5b146047575b005b6058335b90565b6058600054600160a060020a03166032565b6058600054600160a060020a031681565b600160a060020a03166060908152602090f3",
     unlinked_binary: "606060405260008054600160a060020a03191633179055606b8060226000396000f3606060405260e060020a60003504635e01eb5a8114602e578063893d20e81460355780638da5cb5b146047575b005b6058335b90565b6058600054600160a060020a03166032565b6058600054600160a060020a031681565b600160a060020a03166060908152602090f3",
-    address: "0x0e2dd5880baa5087295c3d1f6db0f1d7d9c26560",
+    address: "0xae015a72b078405ee43ffefe557154f2e028a116",
     generated_with: "2.0.6",
     contract_name: "Showme"
   };
@@ -5936,10 +5869,77 @@ var Pudding =
 (function() {
 
   var contract_data = {
+    abi: [{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalanceInEth","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[],"type":"constructor"}],
+    binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101408061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b160043560007360ac611e474c1345c75d465914d517d74f9a81206396e4ee3d6100fb84610092565b6100b160043560243533600160a060020a0316600090815260208190526040812054829010156100c3576100f5565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b92915050565b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac905056",
+    unlinked_binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101408061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b1600435600073__ConvertLib____________________________6396e4ee3d6100fb84610092565b6100b160043560243533600160a060020a0316600090815260208190526040812054829010156100c3576100f5565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b92915050565b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac905056",
+    address: "0x325581b77e421529f5fcf1b93b49013f3acec7b5",
+    generated_with: "2.0.6",
+    contract_name: "MetaCoin"
+  };
+
+  function Contract() {
+    if (Contract.Pudding == null) {
+      throw new Error("MetaCoin error: Please call load() first before creating new instance of this contract.");
+    }
+
+    Contract.Pudding.apply(this, arguments);
+  };
+
+  Contract.load = function(Pudding) {
+    Contract.Pudding = Pudding;
+
+    Pudding.whisk(contract_data, Contract);
+
+    // Return itself for backwards compatibility.
+    return Contract;
+  }
+
+  Contract.new = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("MetaCoin error: Please call load() first before calling new().");
+    }
+
+    return Contract.Pudding.new.apply(Contract, arguments);
+  };
+
+  Contract.at = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("MetaCoin error: lease call load() first before calling at().");
+    }
+
+    return Contract.Pudding.at.apply(Contract, arguments);
+  };
+
+  Contract.deployed = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("MetaCoin error: Please call load() first before calling deployed().");
+    }
+
+    return Contract.Pudding.deployed.apply(Contract, arguments);
+  };
+
+  if (typeof module != "undefined" && typeof module.exports != "undefined") {
+    module.exports = Contract;
+  } else {
+    // There will only be one version of Pudding in the browser,
+    // and we can use that.
+    window.MetaCoin = Contract;
+  }
+
+})();
+;
+
+// Factory "morphs" into a Pudding class.
+// The reasoning is that calling load in each context
+// is cumbersome.
+
+(function() {
+
+  var contract_data = {
     abi: [{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"refundTicket","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"destroy","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"newquota","type":"uint256"}],"name":"changeQuota","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"quota","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"numRegistrants","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[],"name":"buyTicket","outputs":[{"name":"success","type":"bool"}],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_from","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Refund","type":"event"}],
     binary: "606060405260008054600160a060020a031916331781556101f460035560025561025d8061002d6000396000f3606060405236156100615760e060020a6000350463705099b9811461006357806383197ef01461008a5780638da5cb5b146100b4578063a977c71e146100c6578063cebe09c9146100e9578063ec3a6f73146100f2578063edca914c146100fb575b005b61006160043560243560008054600160a060020a0390811633919091161461018257610256565b610061600054600160a060020a039081163391909116141561025b57600054600160a060020a0316ff5b610112600054600160a060020a031681565b610061600435600054600160a060020a039081163391909116146101795761017f565b61011260035481565b61011260025481565b610112600354600254600091901061011c57610176565b6060908152602090f35b600160a060020a03331680825260016020819052604080842034908190556002805490930190925560609283526080919091527fe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c91a15060015b90565b60038190555b50565b600160a060020a038316815260016020526040812054821415610256575030600160a060020a0381163182901061025657600160a060020a038316600083606082818181858883f193505050505060006001600050600085600160a060020a03168152602001908152602001600020600050819055506002600081815054809291906001900391905055507fbb28353e4598c3b9199101a66e0989549b659a59a54d2c27fbb183f1932c8e6d83836040518083600160a060020a031681526020018281526020019250505060405180910390a15b505050565b56",
     unlinked_binary: "606060405260008054600160a060020a031916331781556101f460035560025561025d8061002d6000396000f3606060405236156100615760e060020a6000350463705099b9811461006357806383197ef01461008a5780638da5cb5b146100b4578063a977c71e146100c6578063cebe09c9146100e9578063ec3a6f73146100f2578063edca914c146100fb575b005b61006160043560243560008054600160a060020a0390811633919091161461018257610256565b610061600054600160a060020a039081163391909116141561025b57600054600160a060020a0316ff5b610112600054600160a060020a031681565b610061600435600054600160a060020a039081163391909116146101795761017f565b61011260035481565b61011260025481565b610112600354600254600091901061011c57610176565b6060908152602090f35b600160a060020a03331680825260016020819052604080842034908190556002805490930190925560609283526080919091527fe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c91a15060015b90565b60038190555b50565b600160a060020a038316815260016020526040812054821415610256575030600160a060020a0381163182901061025657600160a060020a038316600083606082818181858883f193505050505060006001600050600085600160a060020a03168152602001908152602001600020600050819055506002600081815054809291906001900391905055507fbb28353e4598c3b9199101a66e0989549b659a59a54d2c27fbb183f1932c8e6d83836040518083600160a060020a031681526020018281526020019250505060405180910390a15b505050565b56",
-    address: "0xb36b2d2d6d805379aa5a543c89e6e06b63fb3395",
+    address: "0xf25bf867bc26b6fcccf41199fbbd86b172feb46d",
     generated_with: "2.0.6",
     contract_name: "Ticketbooth"
   };
@@ -6146,5 +6146,5 @@ if (typeof web3 !== 'undefined') {
 
 Pudding.setWeb3(window.web3);                                 
 
-Pudding.load([ConvertLib, MetaCoin, Showme, Ticketbooth], window);               
+Pudding.load([ConvertLib, Showme, MetaCoin, Ticketbooth], window);               
 
--- environments/development/contracts/ConvertLib.sol.js
+++ environments/development/contracts/ConvertLib.sol.js
@@ -8,7 +8,7 @@
     abi: [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"conversionRate","type":"uint256"}],"name":"convert","outputs":[{"name":"convertedAmount","type":"uint256"}],"type":"function"}],
     binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
     unlinked_binary: "606060405260358060106000396000f3650301c67926cf50606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
-    address: "0xaa0207807f457876b8d596c699dbea1aab9ff137",
+    address: "0x60ac611e474c1345c75d465914d517d74f9a8120",
     generated_with: "2.0.6",
     contract_name: "ConvertLib"
   };
